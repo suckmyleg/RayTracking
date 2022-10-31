09db0309d90c8706708f9cf97dca6286eb28884b
suckmyleg <juanito-146@hotmail.es>
2022-10-31 20:20:59 +0100

    Saving

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ function on_load()
 
 	view3d.innerHTML = "Hail";
 }
+
+function key_pressed()
+{
+
+}
+
+function mouse_position()
+{
+	event = window.event;
+
+	mouse_position = {"x":e.clientX, "y":e.clientY];
+
+	on_mouse_moved(mouse_position)
+
+	var t = setTimeout(mouse_position,100);
+}
+
+function on_mouse_moved(position)
+{
+
+}
