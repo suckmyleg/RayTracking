df4b0f74d277795d9f7fac47715db0933ebdee64
elber <paggiluis@gmail.com>
2022-11-01 02:19:42 +0100

    saving

--- js/main.js
+++ js/main.js
@@ -9,25 +9,91 @@ function on_load()
     DrawView2d(view2d);
 }
 
-
-class VistaJugador {
-    contructor (x, y){
-        this.x = 5;
-        this.y = 225;
+class Point {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
     }
 }
 
-var vista = VistaJugador(0,0);
+class Esquina {
+    constructor(xi, yi, xf, yf){
+        this.xi = xi;
+        this.yi = yi;
+        this.xf = xf;
+        this.yf = yf;
+    }
+}
+
+class VistaJugador {
+    constructor(x, y, angulo){
+        this.x = x;
+        this.y = y;
+        this.angulo = angulo;
+    }
+
+    point (){
+        return {x: this.x, y: this.y}
+    }
+}
+
+var Vista = new VistaJugador(5,225, 50);
 
 function DrawView2d(view2d) {
-    
-    ctx = view2d.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(20, 20);
-    ctx.lineTo(20, 100);
-    ctx.lineTo(70, 100);
-    ctx.stroke();
 
+    ctx = view2d.getContext("2d");
+    
+    //dibujar fondo
+    drawRect(ctx, 0, 0, view2d.width, view2d.height, "white")
+    //dibujar jugador
+    drawLine(ctx, Vista.point(), new Point(5, 399));
+    //dibujar cono de vision
+    drawVision(ctx, Vista.point());
+
+}
+
+function drawVision(ctx){
+    //calcular puntos de colision de la vista con el techo y el suelo
+    puntos = calcularCono();
+    drawLine(ctx, Vista.point(), puntos.techo, "red");
+    drawLine(ctx, Vista.point(), puntos.suelo, "red");
+}
+
+function calcularCono() {
+    tan = getTanFromDegrees(Vista.angulo/2);
+    //punto techo
+    x = Vista.x + (Vista.y/tan);
+    y = 0;
+    puntoTecho = {x: x, y: y};
+    //punto suelo
+    x = Vista.x + (175/tan);
+    y = 399;
+    puntoSuelo = {x: x, y: y};
+
+    puntos = {techo: puntoTecho, suelo: puntoSuelo};
+    return puntos;
+}
+
+function getTanFromDegrees(degrees) {
+    return Math.tan(degrees * Math.PI / 180);
+}
+
+function drawRect(ctx, x, y, w, h, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
+function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
+    if (stroke)
+        ctx.strokeStyle = stroke;
+
+    if (width)
+        ctx.lineWidth = width;
+
+    ctx.beginPath();
+    ctx.moveTo(begin.x+0.5, begin.y+0.5);
+    ctx.lineTo(end.x+0.5, end.y+0.5);
+    ctx.stroke();
 }
 
 function key_pressed()
