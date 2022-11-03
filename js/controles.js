76333610aae15c43802dcaf7079dc940f3f03b9a
elber <paggiluis@gmail.com>
2022-11-03 16:37:04 +0100

    saving

--- js/controles.js
+++ js/controles.js
@@ -5,19 +5,6 @@ Controles = {
     right: false
 }
 
-function onloadVistaAerea(){
-
-    cargarVariables();
-    dibujarFondo(viewAerea);
-    puntoA = new Point(450, 120);
-    puntoB = new Point(450, 240);
-    Pared = new Wall(puntoA, puntoB);
-    Jugador = new Player(new Point(5,5));
-    Pared.draw();
-    Jugador.draw();
-    
-}
-
 document.addEventListener("keydown", function(event){
 
     if (event.key === "w") {
@@ -45,4 +32,3 @@ document.addEventListener("keyup", function(event){
     }
 
 });
-
